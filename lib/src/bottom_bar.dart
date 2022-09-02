f8af33da6197661333009a6f05a30c401cde7fed
danieljorge17 <danieljorge17@hotmail.com>
2022-09-02 15:25:08 -0300

    commit before making the animated container

--- lib/src/bottom_bar.dart
+++ lib/src/bottom_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
 import 'package:rive/rive.dart';
 
 class BottomBarFancyPage extends StatelessWidget {
@@ -7,11 +8,14 @@ class BottomBarFancyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       body: BottomBarFancy(),
     );
   }
 }
+
+RxInt buttonPressed = 0.obs;
+RxInt lastButtonPressed = 0.obs;
 
 class BottomBarFancy extends StatefulWidget {
   const BottomBarFancy({Key? key}) : super(key: key);
@@ -32,7 +36,6 @@ class _BottomBarFancyState extends State<BottomBarFancy> {
       switch ((endIndex - startIndex).abs()) {
         case 0:
           print("Animation not needed");
-
           break;
         case 1:
           _controllerShort.isActive = true;
@@ -74,96 +77,221 @@ class _BottomBarFancyState extends State<BottomBarFancy> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 500),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: List.generate(
+                4,
+                ((index) => ButtonAnimated(index: index)),
+              ),
+            ),
+            Obx(
+              () => Positioned(
+                left: buttonPressed.value == 0 ? -9.8 : null,
+                right: buttonPressed.value == 3 ? -9.8 : null,
+                bottom: 0,
+                height: 100,
+                width: 472,
+                child: SizedBox(
+                  height: 100,
+                  width: 472,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        flex: adjustleftContainer(),
+                        child: SizedBox(
+                          height: 80,
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              AnimatedContainer(
+                                duration: const Duration(milliseconds: 800),
+                                color: const Color(0xFF3F4C73),
+                              ),
+                              Positioned(
+                                right: -1,
+                                child: Container(
+                                  height: 80,
+                                  width: 2,
+                                  color: const Color(0xFF3F4C73),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 100,
+                        width: 140,
+                        child: GestureDetector(
+                          onTap: () {
+                            if (_isPlaying) {
+                              return;
+                            } else {
+                              whichControllerPlaysNow(
+                                  startIndex: lastButtonPressed.value,
+                                  endIndex: buttonPressed.value);
+                              print(
+                                  "LB: ${lastButtonPressed.value}\nBP: ${buttonPressed.value}");
+                              lastButtonPressed.value = buttonPressed.value;
+                            }
+                          },
+                          child: RiveAnimation.asset(
+                            "assets/animations/gap_animation.riv",
+                            fit: BoxFit.contain,
+                            controllers: [
+                              _controllerShort,
+                              _controllerMedium,
+                              _controllerLong
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: adjustRightContainer(),
+                        child: SizedBox(
+                          height: 80,
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              AnimatedContainer(
+                                duration: const Duration(milliseconds: 800),
+                                color: const Color(0xFF3F4C73),
+                              ),
+                              Positioned(
+                                left: -1,
+                                child: Container(
+                                  height: 80,
+                                  width: 2,
+                                  color: const Color(0xFF3F4C73),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: List.generate(
+                4,
+                ((index) => ClickeableArea(index: index)),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          height: 24,
+          color: Colors.black12,
+        )
+      ],
+    );
+  }
+
+  int adjustleftContainer() {
+    switch (buttonPressed.value) {
+      case 0:
+        return 0;
+      case 1:
+        return 7;
+      case 2:
+        return 17;
+      case 3:
+        return 1;
+      default:
+        return 0;
+    }
+  }
+
+  int adjustRightContainer() {
+    switch (buttonPressed.value) {
+      case 0:
+        return 1;
+      case 1:
+        return 22;
+      case 2:
+        return 16;
+      case 3:
+        return 0;
+      default:
+        return 0;
+    }
+  }
+}
+
+class ButtonAnimated extends StatefulWidget {
+  const ButtonAnimated({Key? key, required this.index}) : super(key: key);
+  final int index;
+
+  @override
+  State<ButtonAnimated> createState() => _ButtonAnimatedState();
+}
+
+class _ButtonAnimatedState extends State<ButtonAnimated> {
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => AnimatedContainer(
+        duration: const Duration(milliseconds: 600),
+        curve: Curves.decelerate,
+        alignment: buttonPressed.value == widget.index
+            ? Alignment.topCenter
+            : Alignment.bottomCenter,
         height: 100,
-        width: 472,
-        color: Colors.black,
-        child: GestureDetector(
-          onTap: () {
-            if (_isPlaying) {
-              return;
-            } else {
-              whichControllerPlaysNow(startIndex: 1, endIndex: 3);
-            }
-          },
-          child: RiveAnimation.asset(
-            "assets/animations/gap_animation.riv",
-            //artboard: "only_gap",
-            fit: BoxFit.contain,
-            controllers: [_controllerShort, _controllerMedium, _controllerLong],
-          ),
+        width: 60,
+        color: Colors.transparent,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 600),
+          curve: Curves.decelerate,
+          height: buttonPressed.value == widget.index ? 60 : 30,
+          width: buttonPressed.value == widget.index ? 60 : 30,
+          decoration: const BoxDecoration(
+              color: Color(0xFF3F4C73), shape: BoxShape.circle),
         ),
       ),
     );
   }
+}
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Center(
-  //     child: AnimatedContainer(
-  //       duration: const Duration(milliseconds: 500),
-  //       height: 100,
-  //       width: 472,
-  //       color: Colors.black,
-  //       child: Row(
-  //         crossAxisAlignment: CrossAxisAlignment.end,
-  //         children: [
-  //           Flexible(
-  //             flex: 5,
-  //             child: SizedBox(
-  //               height: 80,
-  //               child: Stack(
-  //                 clipBehavior: Clip.none,
-  //                 children: [
-  //                   Container(
-  //                     color: Colors.white,
-  //                   ),
-  //                   Positioned(
-  //                     right: -1,
-  //                     child: Container(
-  //                       height: 80,
-  //                       width: 2,
-  //                       color: Colors.white,
-  //                     ),
-  //                   )
-  //                 ],
-  //               ),
-  //             ),
-  //           ),
-  //           const RiveAnimation.asset(
-  //             "assets/animations/gap_animation.riv",
-  //             artboard: "only_gap",
-  //             fit: BoxFit.cover,
-  //             //controllers: [],
-  //           ),
-  //           Flexible(
-  //             flex: 1,
-  //             child: SizedBox(
-  //               height: 80,
-  //               child: Stack(
-  //                 clipBehavior: Clip.none,
-  //                 children: [
-  //                   Container(
-  //                     color: Colors.white,
-  //                   ),
-  //                   Positioned(
-  //                     left: -1,
-  //                     child: Container(
-  //                       height: 80,
-  //                       width: 2,
-  //                       color: Colors.white,
-  //                     ),
-  //                   )
-  //                 ],
-  //               ),
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
+class ClickeableArea extends StatefulWidget {
+  const ClickeableArea({Key? key, required this.index}) : super(key: key);
+  final int index;
 
+  @override
+  State<ClickeableArea> createState() => _ClickeableAreaState();
+}
+
+class _ClickeableAreaState extends State<ClickeableArea> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("Button index: ${widget.index}");
+        buttonPressed.value = widget.index;
+        setState(() {});
+      },
+      child: Obx(
+        () => Container(
+          alignment: buttonPressed.value == widget.index
+              ? Alignment.topCenter
+              : Alignment.bottomCenter,
+          height: 100,
+          width: 60,
+          color: Colors.transparent,
+        ),
+      ),
+    );
+  }
 }
